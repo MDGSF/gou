9068a9dedbd233c4a08d5bb0f06068908c7e07a3
huangjian <1342042894@qq.com>
2018-07-19 17:21:27 +0800

    rotate to write file

--- log/example/mwriter/main.go
+++ log/example/mwriter/main.go
@@ -6,11 +6,10 @@ import (
 	"github.com/MDGSF/utils/log/mwriter"
 )
 
-const M1 = 10 * 1024 * 1024
 const M3 = 30 * 1024 * 1024
 
 func main() {
-	w := mwriter.New("log.txt", M1, M3)
+	w := mwriter.New("log.txt", M3)
 	log.SetOutput(w)
 	log.SetLevel(log.VerboseLevel)
 	log.SetIsTerminal(log.NotTerminal)
--- log/mwriter/rotatewriter.go
+++ log/mwriter/rotatewriter.go
@@ -4,35 +4,34 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
+
+	"github.com/MDGSF/utils/log"
 )
 
 type RotateWriter struct {
 	lock     sync.Mutex
 	filename string // should be set to the actual filename
 	fp       *os.File
-
-	count    int
-	maxlsize int64
-	maxrsize int64
+	maxsize  int64
 }
 
 // Make a new RotateWriter. Return nil if error occurs during setup.
-func New(filename string, maxlsize int, maxrsize int) *RotateWriter {
-
+func New(filename string, maxsize int) *RotateWriter {
 	w := &RotateWriter{
 		filename: filename,
-		maxlsize: int64(maxlsize),
-		maxrsize: int64(maxrsize),
+		maxsize:  int64(maxsize),
 	}
-
-	var err error
-	w.fp, err = os.Create(w.filename)
-	if err != nil {
-		return nil
-	}
-
+	w.createLogFile()
 	return w
+}
+
+func (w *RotateWriter) createLogFile() {
+	var err error
+	w.fp, err = os.OpenFile(w.filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Error("create log file [%v] failed, err = %v", w.filename, err)
+		return
+	}
 }
 
 // Write satisfies the io.Writer interface.
@@ -43,46 +42,15 @@ func (w *RotateWriter) Write(output []byte) (int, error) {
 }
 
 func (w *RotateWriter) write(output []byte) (int, error) {
-	w.count++
-	if w.count > 10000 {
-		w.count = 0
-		if w.fileIsTooBig() {
-			w.reduceFileSize()
-		}
+	if w.needRotate() {
+		w.reduceFileSize()
 	}
 
 	return w.fp.Write(output)
 }
 
-func (w *RotateWriter) curFileSize() int64 {
-	fileInfo, err := w.fp.Stat()
-	if err != nil {
-		fmt.Printf("err = %v", err)
-		return 0
-	}
-	return fileInfo.Size()
-}
-
-func (w *RotateWriter) fileIsTooBig() bool {
-	if w.curFileSize() > w.maxrsize {
-		return true
-	}
-	return false
-}
-
 func (w *RotateWriter) reduceFileSize() {
-	_, err := w.fp.Seek(w.curFileSize()-w.maxlsize, 0)
-	if err != nil {
-		fmt.Println("seek failed, err = ", err)
-		return
-	}
-
-	b := make([]byte, w.maxlsize)
-	_, err = w.fp.Read(b)
-	if err != nil {
-		fmt.Println("read failed, err = ", err)
-		return
-	}
+	var err error
 
 	// Close existing file if open
 	if w.fp != nil {
@@ -104,48 +72,22 @@ func (w *RotateWriter) reduceFileSize() {
 	}
 
 	// Create a file.
-	w.fp, err = os.Create(w.filename)
-	if err != nil {
-		fmt.Println("create file failed, err = ", err)
-		return
-	}
-
-	_, err = w.fp.Write(b)
-	if err != nil {
-		fmt.Println("write to file failed, err = ", err)
-		return
-	}
-
+	w.createLogFile()
 	return
-
 }
 
-// Perform the actual act of rotating and reopening file.
-func (w *RotateWriter) Rotate() (err error) {
-	w.lock.Lock()
-	defer w.lock.Unlock()
-	return w.rotate()
+func (w *RotateWriter) curFileSize() int64 {
+	fileInfo, err := w.fp.Stat()
+	if err != nil {
+		fmt.Printf("err = %v", err)
+		return 0
+	}
+	return fileInfo.Size()
 }
 
-func (w *RotateWriter) rotate() (err error) {
-	// Close existing file if open
-	if w.fp != nil {
-		err = w.fp.Close()
-		w.fp = nil
-		if err != nil {
-			return
-		}
+func (w *RotateWriter) needRotate() bool {
+	if w.curFileSize() > w.maxsize {
+		return true
 	}
-	// Rename dest file if it already exists
-	_, err = os.Stat(w.filename)
-	if err == nil {
-		err = os.Rename(w.filename, w.filename+"."+time.Now().Format(time.RFC3339))
-		if err != nil {
-			return
-		}
-	}
-
-	// Create a file.
-	w.fp, err = os.Create(w.filename)
-	return
+	return false
 }
